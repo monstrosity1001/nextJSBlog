daceeac2a7d6e059281fd8e582aec5059d4267a4
monstrosity1001 <111359902+monstrosity1001@users.noreply.github.com>
2023-07-10 17:45:28 -0700

    Finished and should be working now

--- pages/posts/TicTacToe.jsx
+++ pages/posts/TicTacToe.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import styles from "../../styles/TicTacToe.module.css";
-import "../../styles/global.css";
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
