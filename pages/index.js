c6a47bfdab396934889082160d6e0e4fa9d10a43
monstrosity1001 <111359902+monstrosity1001@users.noreply.github.com>
2023-07-12 13:50:08 -0700

    Fixed first page to have basic parts but not blog links

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from "next/head";
 import Link from "next/link";
-import Layout, {siteTitle} from '../components/layout';
+import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,8 +25,22 @@ export default function Home() {
           I built this with Next.js!
         </p>
         <Link href="/posts/first-post">
-            <button>Go to my first post</button>
+          <button>Go to my first post</button>
         </Link>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
