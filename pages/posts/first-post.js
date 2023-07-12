d487cc8be940a081648e13ae43d9a1c4210de211
monstrosity1001 <111359902+monstrosity1001@users.noreply.github.com>
2023-07-12 11:53:20 -0700

    Centred the text on first post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,18 +1,13 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
     <Head>
       <title>First Post</title>
     </Head>
-    <Script
-      src="https://connect.facebook.net/en_US/sdk.js"
-      strategy="lazyOnLoad"
-      onLoad={() => console.log('script loaded correctly, window.FB now exists')}
-    />
     <h1>First Post!!!</h1>
     <h2>
       Play <Link href="/posts/TicTacToe">TicTacToe</Link>
@@ -20,6 +15,6 @@ export default function FirstPost() {
     <h3>
       <Link href="/">Back to home</Link>
     </h3>
-    </>
+    </Layout>
   );
 }
