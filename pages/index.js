57d3e7551aa816a2cecc00a641f41e4020d64369
monstrosity1001 <111359902+monstrosity1001@users.noreply.github.com>
2023-07-12 12:38:02 -0700

    Before adding blog content and including data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
@@ -13,6 +14,9 @@ export default function Home() {
         <p>
           I built this with Next.js!
         </p>
+        <Link href="/posts/first-post">
+            <button>Go to my first post</button>
+        </Link>
       </section>
     </Layout>
   );
