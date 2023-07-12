a112d9f3e8a3bd50c4695bfa3cc1ff7ad20e8090
monstrosity1001 <111359902+monstrosity1001@users.noreply.github.com>
2023-07-12 15:38:18 -0700

    Pre changing date

---
+++ components/date.js
@@ -0,0 +1,21 @@
+import {parseISO, format} from 'date-fns';
+
+export default function Date({dateString}) {
+    if (!dateString) {
+        console.warn('dateString is not defined or null');
+        return null; 
+    }
+
+    const date = parseISO({dateString});
+
+    if (isNaN(date)) {
+        console.warn(`dateString "${dateString}" is not a valid date.`);
+        return null;
+    }
+
+    return (
+        <time dateTime={dateString}>
+            {format(date, 'LLLL d, yyyy')}
+        </time>
+    );
+}
